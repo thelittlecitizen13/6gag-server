1697aae21a067b9108801b3d742ce0b8bfe640dd
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-30 16:09:49 +0300

    app.js added

---
+++ data/data.js
@@ -0,0 +1,16 @@
+const images = [
+    {
+        id: 1,
+        alt: "debug-meme",
+        path: require('../gags/debug.jpg'),
+        title: "I Will Fix You"
+    },
+    {
+        id: 2,
+        alt: "stackoverflow-gag",
+        path: require('../gags/stackoverflow-gag.jpg'),
+        title: "Shit is true"
+    }
+]
+
+exports.images = images;
